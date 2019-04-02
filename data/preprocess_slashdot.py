f1020d1e658151a1694977fe129de619bd827e3b
PRATIK MAZUMDER <pratikmazumder72@gmail.com>
2019-04-02 21:04:59 +0530

    Update preprocess_slashdot.py

--- data/preprocess_slashdot.py
+++ data/preprocess_slashdot.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 import tqdm
-labelfile =  open("../../data/slashdot/new/SLASHDOT-F.arff","r")
+labelfile =  open("./data/slashdot/SLASHDOT-F.arff","r")
 featlist = []
 labellist = []
 cnt = 0
